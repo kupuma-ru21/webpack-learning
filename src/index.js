fe6619d6e7cbedc08cc0645a5144bcefd5042c40
kupuma-ru21 <kupumaru21@gmail.com>
2020-03-22 13:54:43 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import { Nijou } from './utilities';
 
 function component() {
   const element = document.createElement('div');
   const array = ['Hello', 'Webpack', '!!'];
   element.innerHTML = _.join(array, ' ');
+  console.log(Nijou(2));
   return element;
 }
 
