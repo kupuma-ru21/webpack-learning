e67b1d37d866aabde9b4d8f971815a73edac7254
kupuma-ru21 <kupumaru21@gmail.com>
2020-03-22 13:21:26 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,15 @@ const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // entryファイルは相対パスで指定
+  // outputは絶対パスで指定
   output: {
     filename: 'main.js',
-    path: outputPath,
+    path: outputPath, // main.jsまでの絶対パス
+  },
+  //webpack-dev-server 実行時の設定 (ファイルの起動時や変更時の監視を行う)
+  devServer: {
+    // npx webpack-dev-server --open 実行時 絶対パス(outputPath)で dist配下のindex.htmlを起動するよう設定
+    contentBase: outputPath,
   },
 };
